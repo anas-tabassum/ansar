4ac4058680563da8f184c878b026f6755293adc0
Anas Tabassum <work632294@gmail.com>
2024-10-26 17:42:25 +0500

    Hajj cards finalized

--- src/components/HajjCard.jsx
+++ src/components/HajjCard.jsx
@@ -2,25 +2,18 @@ import React from "react";
 import styles from "../styles/HajjCard.module.css";
 import card1 from "../media/card1.png";
 
-const HajjCard = () => {
+const HajjCard = ({ item }) => {
   return (
     <div>
-      <div className={`w-[100%] ${styles.flipCard}`}>
-        <div className={styles.flipCardInner}>
+      <div
+        className={`w-[100%] !h-[20rem] md:h-[auto] mb-12 md:mb-0 ${styles.flipCard}`}
+      >
+        <div className={`${styles.flipCardInner} min-h-auto md:min-h-[20rem]`}>
           <div className={styles.flipCardFront}>
-            <img src={card1} alt="Card1" />
+            <img src={require(`../media/${item.image}`)} />
           </div>
           <div className={styles.flipCardBack}>
-            {/* <p className={styles.title}>BACK</p> */}
-            <p>
-              Le programme 1 commence à Médine, où vous serez logé dans un hôtel
-              à moins d'1 km de la mosquée du Prophète Muhammad ﷺ. Ensuite, vous
-              serez transféré dans un hôtel à moins d'1 km de la Kaaba à La
-              Mecque. Vous y serez hébergé et conserverez votre chambre à La
-              Mecque pendant les Manasik. Après les Manasik, vous resterez à La
-              Mecque jusqu'à votre transfert à l'aéroport de Djeddah pour votre
-              vol de départ.
-            </p>
+            <p className="px-4 py-2 text-justify">{item.text}</p>
           </div>
         </div>
       </div>
--- src/pages/HajjPackages.jsx
+++ src/pages/HajjPackages.jsx
@@ -1,9 +1,29 @@
 import HajjCard from "../components/HajjCard";
 
 const HajjPackages = () => {
+  let data = [
+    {
+      id: 1,
+      image: "card1.png",
+      text: "Le programme 1 commence à Médine, où vous serez logé dans un hôtel à moins d'1 km de la mosquée du Prophète Muhammad ﷺ. Ensuite, vous serez transféré dans un hôtel à moins d'1 km de la Kaaba à La Mecque. Vous y serez hébergé et conserverez votre chambre à La Mecque pendant les Manasik. Après les Manasik, vous resterez à La Mecque jusqu'à votre transfert à l'aéroport de Djeddah pour votre vol de départ.",
+    },
+    {
+      id: 2,
+      image: "card2.png",
+      text: "Le programme 2 commence à Médine, où vous serez logé dans un hôtel à moins de 2 km de la mosquée du Prophète Muhammad ﷺ. Ensuite, vous serez transféré dans un hôtel à moins de 2 km de la Kaaba à La Mecque. Vous y serez hébergé et conserverez votre chambre à La Mecque pendant les Manasik. Après les Manasik, vous resterez à La Mecque jusqu'à votre transfert à l'aéroport de Djeddah pour votre vol de départ.",
+    },
+    {
+      id: 3,
+      image: "card3.png",
+      text: "Le programme 3 commence à Médine, où vous serez logé dans un hôtel à moins de 3 km de la mosquée du Prophète Muhammad ﷺ. Ensuite, vous serez transféré dans un hôtel à moins de 3 km de la Kaaba à La Mecque. Vous y serez hébergé et conserverez votre chambre à La Mecque pendant les Manasik. Après les Manasik, vous resterez à La Mecque jusqu'à votre transfert à l'aéroport de Djeddah pour votre vol de départ.",
+    },
+  ];
   return (
-    <div>
-      <HajjCard />
+    <div className="">
+      <h1 className="text-center text-4xl mb-12">Forfaits Hajj</h1>
+      <div className="flex flex-col items-center justify-evenly md:flex-row">
+        {data && data.map((item) => <HajjCard item={item} />)}
+      </div>
     </div>
   );
 };

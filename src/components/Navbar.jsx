b4f4fea97aaeae218ad7938c12a25bc5f74989ad
Anas Tabassum <wor632294@gmail.com>
2025-02-08 22:50:23 +0500

    fix nav height

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -31,7 +31,7 @@ const Navbar = () => {
   return (
     <div>
       <nav className="bg-white border-gray-200 dark:bg-gray-900">
-        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto custom-margin-top">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto custom-margin-top h-[9rem]">
           <NavLink
             to="/"
             className="flex items-center space-x-3 rtl:space-x-reverse"

0d2ec2d6086cacf99e795431123fe356a3bb90b8
boubacaride <39181066+boubacaride@users.noreply.github.com>
2025-07-06 02:03:30 -0400

    Update HomeBackground.jsx

--- src/components/HomeBackground.jsx
+++ src/components/HomeBackground.jsx
@@ -7,13 +7,14 @@ const HomeBackground = () => {
         <source src="https://ansarv1.s3.us-east-2.amazonaws.com/videos/camel.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
-  <div className={`${styles.custom_font_ubuntu} text-white my-48 ml-8`}>
-        <h1 className="text-3xl md:text-6xl" style={{fontFamily: 'Montserrat', fontWeight: 800, fontStyle: 'italic'}}>
+ <div className={`${styles.custom_font_ubuntu} text-white my-48 ml-8`}>
+        <h1 className="text-3xl md:text-6xl" style={{fontFamily: 'Montserrat', fontWeight: 900, fontStyle: 'italic'}}>
           <span style={{color: '#1919ff'}}>Ansar</span> <span style={{color: '#ff7f00'}}>Voyage</span>
         </h1>
-        <p className="text-xl md:text-3xl" style={{fontFamily: 'Montserrat', fontWeight: 400, fontStyle: 'italic'}}>
+        <p className="text-lg md:text-2xl" style={{fontFamily: 'Montserrat', fontWeight: 400, fontStyle: 'italic'}}>
           Le Chemin Vers Votre But
         </p>
+        <br />
         <br />
         <p className="text-xl md:text-3xl" style={{fontFamily: 'Montserrat', fontWeight: 400, fontStyle: 'italic'}}>
           Nous sommes là pour honorer votre appel spirituel

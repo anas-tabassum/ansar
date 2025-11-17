f671071271f1c3ae474a3a7b31aa559fc68ea0e7
boubacaride <39181066+boubacaride@users.noreply.github.com>
2025-11-17 12:40:10 -0500

    Add Quill CSS for rich text editor

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import 'quill/dist/quill.snow.css';
 import App from "./App";
 import store from "./store/store";
 import { Provider } from "react-redux";
